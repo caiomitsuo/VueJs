6568e7b39e542b1431088a0f09f20a0b3a8e9076
Caio Mitsuo <caiomitsuo@164-ENG-7YWNH-N.belagricola.local>
2020-07-29 14:58:41 -0300

    Primeiro commit

---
+++ vuex-playlist/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <product-list-one> </product-list-one>
+    <product-list-two> </product-list-two>
+  </div>
+</template>
+
+<script>
+import ProductListOne from "./components/ProductListOne";
+import ProductListTwo from "./components/ProductListTwo";
+
+export default {
+  components: {
+    "product-list-one": ProductListOne,
+    "product-list-two": ProductListTwo
+  },
+
+  name: "app",
+  data() {
+    return {
+    };
+  }
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+body{
+  font-family:Roboto;
+  color:#555;
+}
+</style>
---
+++ vuex-playlist/src/components/ProductListOne.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="product-list-one">
+    <h2>Product List One</h2>
+    <ul>
+      <li v-for="product in saleProducts" :key="product">
+        <span class="name">{{ product.name }}</span>
+        <span class="price">${{ product.price }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    products(){
+      return this.$store.state.products
+    },
+    saleProducts(){
+    var saleProducts = this.$store.state.products.map( product => {
+      return{
+        name:'**' + product.name + '**',
+        price:product.price / 2,
+      }
+    });
+      return saleProducts
+    }
+  }
+};
+</script>
+<style>
+#product-list-one {
+  background: #fff8b1;
+  margin-bottom: 30px;
+  padding: 10px 20px;
+}
+#product-list-one ul {
+  padding: 0;
+  list-style-type: none;
+}
+
+#product-list-one li {
+  margin-right: 10px;
+  margin-top: 10px;
+  padding: 20px;
+  background: rgba(255, 255, 255, 0.7);
+}
+.price {
+  font-weight: bold;
+  color: #860ce8;
+  display: block;
+}
+</style>
